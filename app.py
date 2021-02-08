1915e4ac599b9269dfc0c36eb710ee91310c9393
Default <francois.semecurbe@gmail.com>
2021-02-08 17:21:48 +0000

    add app

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
-
 import pandas as pd
+import igraph
+
+
+
+
+
 
 #load data
 flow = pd.read_csv('data/trade_2018.csv')
@@ -20,24 +25,34 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+
 app.layout = html.Div([
-   # html.Div(["Input: ",
-   #           dcc.Input(id='my-input', value=available_indicators[0], type='text')]),
+
  dcc.Dropdown(
         id='my-input',
-        #options=[{'label': i, 'value': i} for i in available_indicators[:20]],
         options=[{'label': 'France', 'value': 'FRA'}, {'label': 'Germany', 'value':'DEU'}],
         value='FRA'
     ),
-    html.Div(id='my-output')
+   
+    html.Div(id='my-output'),
+    html.Div([
+    html.Div([dcc.Graph(id='map_ex'),], className='six columns'),
+    html.Div([dcc.Graph(id='map_im'),], className='six columns')
+    ], className='row')          
+             
 ])
 
 
 @app.callback(Output('my-output', 'children'),
+              Output('map_ex', 'figure'),
+              Output('map_im', 'figure'),
               Input('my-input', 'value'))
     
 def update_output_div(input_value):
-    return 'Output: {}'.format(input_value)
+    
+    return ('Output: {}'.format(input_value),
+            px.scatter_geo(flow[flow.j_country==input_value], locations="i_country", size="v", projection="natural earth"),
+            px.scatter_geo(flow[flow.i_country==input_value], locations="j_country", size="v", projection="natural earth"))
 
 if __name__ == '__main__':
     app.run_server(debug=True)
