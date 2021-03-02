9a29543f4725de7fd61f2e048c077aa9682ee494
fsemecurbe <francoissemecurbe@gmail.com>
2021-03-02 16:14:56 +0100

    Create code.sh

---
+++ code.sh
@@ -0,0 +1,31 @@
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+
+sudo apt install python3-pip
+sudo apt-get install python3-pip
+export PATH="$HOME/.local/bin:$PATH"
+
+python3.7 -m pip install cython
+python3.7 -m pip install streamlit geopandas
+sudo apt-get install emacs
+sudo apt install git
+
+export PATH="$HOME/.local/bin:$PATH"
+
+apt-get install -y software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
+apt-get update
+
+apt-get install build-essential libpq-dev libssl-dev openssl libffi-dev zlib1g-dev
+apt-get install python3-pip python3.7-dev
+apt-get install python3.7
+
+alias python='/usr/bin/python3.7'
+alias python3='/usr/bin/python3.7'
+
+python -m pip install -U #pour installe geopandas
+python3.7 -m pip install cython
+python3.7 -m pip install streamlit geopandas
+
+sudo apt-get install emacs
